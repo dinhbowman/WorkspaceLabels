e3a182a3fce614e36805a65dc26fd4d72d65c44f
Dinh Bowman <dinh@vagueindustries.com>
2011-07-13 17:36:25 -0700

    Basic functionality is there now. CSS isn't working yet though.

--- WorkspaceLabels@VagueIndustries/extension.js
+++ WorkspaceLabels@VagueIndustries/extension.js
@@ -37,21 +37,32 @@ WorkSpaceLabel.prototype = {
         this.menu.passEvents = true;
         this.actor.y_fill = true;
 
+        // Initialize the set of workspace labels
+        this.labels = [];
+        for (let i=0; i < global.screen.n_workspaces; i++) {
+            this.labels[i] = "Workspace " + (i+1).toString();
+        }
+
         // Create the primary visual element
         this._label = new St.Label({ text: 'Workspace Label',
                                      style_class: 'workspace-label' });
         this.actor.set_child(this._label);
 
         // Create the Pop-Out layout
-        this._layout = new St.BoxLayout({ name: 'workspaceLabel' });
+        this._layout = new St.BoxLayout({ name: 'workspaceLabel',
+                                          style_class: 'workspace-layout'});
+        //this._layout.set_psuedo_style_class('workspace-layout');
         this.menu.addActor(this._layout);
 
         // Create the Text Entry
-        this._entry = new St.Entry();
+        this._entry = new St.Entry({ style_class: 'workspace-entry' });
         this._layout.add(this._entry);
 
         // Set the label to be responsive to <ESC> and <CR>
         this._entry.clutter_text.connect('key-press-event', Lang.bind(this, this._onKeyPressEvent));
+
+        // When we lose focus: Close the editing window
+        this._entry.clutter_text.connect('key-focus-out', Lang.bind(this, this.close));
 
         // Add the label to the center of the Top Bar / Panel
         Main.panel._centerBox.add(this.actor, { x_fill: true, y_fill: true } );
@@ -61,19 +72,50 @@ WorkSpaceLabel.prototype = {
         global.screen.connect_after('workspace-removed', Lang.bind(this, this._updateOnRemove));
         global.screen.connect_after('workspace-switched', Lang.bind(this, this._updateOnSwitch));
 
+        // Destroy when necessary
+        this.actor.connect_after('destroy', Lang.bind(this, this._onDestroy));
+
         // Initialize our focus references
         this._lastFocus = undefined;
         this._lastFocusMode = undefined;
+
+        // Sync our labels with the workspace
+        this._syncLabel();
+
+    },
+
+    _syncLabel: function() {
+        /*
+         * Match the workspace label with the displayed workspace.
+         */
+
+        // Figure out which workspace we are in.
+        let curIndex = global.screen.get_active_workspace().index();
+
+        // Update the label
+        this._label.set_text(this.labels[curIndex]);
+    },
+
+    _setLabel: function(newText) {
+        /*
+         * Set the label text so it is stored in our little database.
+         */
+        // Figure out which workspace we are in.
+        let curIndex = global.screen.get_active_workspace().index();
+
+        // Save it in our database
+        this.labels[curIndex] = newText;
+
+        // Set the label to the new text
+        this._label.set_text(newText);
     },
 
     _onButtonPress: function(actor, event) {
-        global.log("ButtonPress");
         this.toggle();
         return true;
     },
 
     _onKeyPressEvent: function(actor, event) {
-        global.log("KeyPressEvent");
         if ( (event.get_key_symbol() == Clutter.Escape) ||
              (event.get_key_symbol() == Clutter.Return) ) {
             this.close();
@@ -82,7 +124,7 @@ WorkSpaceLabel.prototype = {
 
         // Make the entry text match the label text
         let newText = this._entry.get_text();
-        this._label.set_text(newText);
+        this._setLabel(newText);
 
         return false;
     },
@@ -116,9 +158,10 @@ WorkSpaceLabel.prototype = {
     close: function() {
         // Make the entry text match the label text
         let newText = this._entry.get_text();
-        this._label.set_text(newText);
+        this._setLabel(newText);
 
         // Restore the old focus state
+        // ToDo: This doesn't work!?
         global.stage.set_key_focus(this._lastFocus);
         global.stage_input_mode = this._lastFocusMode;
 
@@ -128,6 +171,7 @@ WorkSpaceLabel.prototype = {
 
     _updateOnSwitch: function() {
         global.log("Workspace Switch");
+        this._syncLabel();
         return false;
     },
 
@@ -138,6 +182,10 @@ WorkSpaceLabel.prototype = {
 
     _updateOnAdd: function() {
         global.log("Workspace Add");
+
+        // Figure out how many workspaces there are.
+        totalWorkspaces = global.screen.n_workspaces;
+        this.labels[totalWorkspaces] = "Workspace " + (totalWorkspaces+1).toString();
         return false;
     },
 
